a6b08e2fa1075263acbb824322a8f395b9a1e717
Jesus Laya <jesusgerard2008@gmail.com>
2019-07-29 09:49:56 -0400

    Comentado archivo lenguaje no existentes

--- lang.php
+++ lang.php
@@ -10,11 +10,11 @@ if(isset($_GET['lang'])){
 		include "lang/".$_GET['lang']."/menu.php";
 		include "lang/".$_GET['lang']."/language.php";
 		include "lang/".$_GET['lang']."/home.php";
-		include "lang/".$_GET['lang']."/about.php";
+		/*include "lang/".$_GET['lang']."/about.php";
 		include "lang/".$_GET['lang']."/work-with-me.php";
 		include "lang/".$_GET['lang']."/programs.php";
 		include "lang/".$_GET['lang']."/forense.php";
-        include "lang/".$_GET['lang']."/contact.php";
+        include "lang/".$_GET['lang']."/contact.php"*/;
 		include "lang/".$_GET['lang']."/footer.php";
 		
 	}else{
@@ -24,11 +24,11 @@ if(isset($_GET['lang'])){
 		include "lang/en/menu.php";
 		include "lang/en/language.php";
 		include "lang/en/home.php";
-		include "lang/en/about.php";
+		/*include "lang/en/about.php";
 		include "lang/en/work-with-me.php";
 		include "lang/en/programs.php";
 		include "lang/en/forense.php";
-        include "lang/en/contact.php";
+        include "lang/en/contact.php";*/
 		include "lang/en/footer.php";
 		
 	}
