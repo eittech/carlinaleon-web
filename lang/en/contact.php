19a55d741c803de6963f88559da9fac575128dfd
Jesus Laya <jesusgerard2008@gmail.com>
2019-07-31 10:40:56 -0400

    Nuevos terminos de traduccion de contacto

--- lang/en/contact.php
+++ lang/en/contact.php
@@ -3,22 +3,22 @@
 // Textos de datos
 
 // Contact
-$lang['contact']['content_1'] = "Contáctame";
-$lang['contact']['content_2'] = "Información";
+$lang['contact']['content_1'] = "Contact me";
+$lang['contact']['content_2'] = "Information";
 $lang['contact']['content_3'] = "Find my virtual office:";
 $lang['contact']['content_4'] = "Email";
 $lang['contact']['content_5'] = "Phone";
 $lang['contact']['content_6'] = "Fax";
-$lang['contact']['content_7'] = "Envíanos un mensaje";
+$lang['contact']['content_7'] = "Send us a message";
 $lang['contact']['content_8'] = "Your name";
 $lang['contact']['content_9'] = "Your email";
 $lang['contact']['content_10'] = "Subject";
 $lang['contact']['content_11'] = "Message";
 $lang['contact']['content_12'] = "Send Message";
-$lang['contact']['content_13'] = "Localización";
-$lang['contact']['content_14'] = "En el mapa";
-$lang['contact']['content_15'] = "Dirección:";
+$lang['contact']['content_13'] = "Location";
+$lang['contact']['content_14'] = "On the map";
+$lang['contact']['content_15'] = "Direction:";
 $lang['contact']['content_16'] = "80 Fifth Avenue, in Suite 1004, N.Y., N.Y. 10011 (By appointment only, no walks-in, please)";
-$lang['contact']['content_17'] = "Encuentrame aqui:";
+$lang['contact']['content_17'] = "Find me here:";
 
 ?>
