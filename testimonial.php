b1e02c89bdf5f3f016492e37d9993a3116853a2e
Jesus Laya <jesusgerard2008@gmail.com>
2019-07-31 17:36:25 -0400

    Comienzo de cambios en base a responsive

--- testimonial.php
+++ testimonial.php
@@ -1,5 +1,7 @@
-<div class="section mcb-section " style="padding-top:40px; padding-bottom:0px; background-color:#f0f1f2">
-    <div class="section_wrapper mcb-section-inner">
+<link rel='stylesheet' href='css/media_screen/testimonial.css'>
+
+<div class="container section mcb-section " style="padding-top:40px; padding-bottom:0px; background-color:#f0f1f2">
+    <div class="row section_wrapper mcb-section-inner">
         <div class="wrap mcb-wrap one clearfix">
             <div class="section mcb-section" style="padding-top:30px; padding-bottom:50px; background-repeat:no-repeat;">
                 <div class="column mcb-column one column_column">
@@ -17,70 +19,74 @@
                 <ul>
                     <li class="slide">
                         <p class="carousel-caption">
-                        <div class="idea_box">
-                            <div class="icon">
-                            <i class="icon-quote"></i>
+                            <div class="idea_box col-sm">
+                                <div class="icon">
+                                <i class="icon-quote"></i>
+                                </div>
+                                <div class="desc" style="font-size: 18px;line-height: 25px;">
+                                <?php echo $lang['home']['content_test_2']; ?>
+                                </div>
+                                <br/>
+                                <span class="date"><span style="font-size: 24px;font-weight: 600; color: #5b258f;">Margaret Della</span></span>
+                                <br>
+                                <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;"> Directora ejecutiva - Kingsbridge Heights Community Center.</span>
                             </div>
-                            <div class="desc" style="font-size: 18px;line-height: 25px;">
-                            <?php echo $lang['home']['content_test_2']; ?>
-                            </div>
-                            <br/>
-                            <span class="date"><span style="font-size: 24px;font-weight: 600; color: #5b258f;">Margaret Della</span></span>
-                            <br>
-                            <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;"> Directora ejecutiva - Kingsbridge Heights Community Center.</span>
-                            </p>
-                        </div>
+                        </p>
                     </li>
                     <li class="slide">
                         <p class="carousel-caption">
-                        <div class="idea_box">
-                            <div class="icon">
-                            <i class="icon-quote"></i>
+                            <div class="idea_box col-sm">
+                                <div class="icon">
+                                <i class="icon-quote"></i>
+                                </div>
+                                <div class="desc" style="font-size: 17.5px;line-height: 25px;">
+                                <?php echo $lang['home']['content_test_3']; ?>
+                                </div>
+                                <br/>
+                                <span class="date"><span style="font-size: 24px;font-weight: 600; color: #5b258f;">Karina Aybar</span></span>
+                                <br>
+                                <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;"> <?php echo $lang['home']['content_test_4']; ?></span>
                             </div>
-                            <div class="desc" style="font-size: 17.5px;line-height: 25px;">
-                            <?php echo $lang['home']['content_test_3']; ?>
-                            </div>
-                            <br/>
-                            <span class="date"><span style="font-size: 24px;font-weight: 600; color: #5b258f;">Karina Aybar</span></span>
-                            <br>
-                            <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;"> <?php echo $lang['home']['content_test_4']; ?></span>
-                            </p>
-                        </div>
+                        </p>
                     </li>
                     <li class="slide">
                         <p class="carousel-caption">
-                        <div class="idea_box">
-                            <div class="icon">
-                            <i class="icon-quote"></i>
+                            <div class="idea_box col-sm">
+                                <div class="icon">
+                                <i class="icon-quote"></i>
+                                </div>
+                                <div class="desc" style="font-size: 18px;line-height: 25px;">
+                                <?php echo $lang['home']['content_test_5']; ?>
+                                </div>
+                                <br/>
+                                <span class="date"><span style="font-size: 24px;font-weight: 600; color: #5b258f;">Ireen Ninonuevo</span></span>
+                                <br>
+                                <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;">  <?php echo $lang['home']['content_test_6']; ?></span>
                             </div>
-                            <div class="desc" style="font-size: 18px;line-height: 25px;">
-                            <?php echo $lang['home']['content_test_5']; ?>
-                            </div>
-                            <br/>
-                            <span class="date"><span style="font-size: 24px;font-weight: 600; color: #5b258f;">Ireen Ninonuevo</span></span>
-                            <br>
-                            <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;">  <?php echo $lang['home']['content_test_6']; ?></span>
-                            </p>
-                        </div>
+                        </p>
                     </li>
                     <li class="slide">
                         <p class="carousel-caption">
-                        <div class="idea_box">
-                            <div class="icon">
-                            <i class="icon-quote"></i>
+                            <div class="idea_box col-sm">
+                                <div class="icon">
+                                <i class="icon-quote"></i>
+                                </div>
+                                <div class="desc" style="font-size: 18px;line-height: 25px;">
+                                <?php echo $lang['home']['content_test_7']; ?>
+                                </div>
+                                <br/>
+                                <span class="date"><span style="font-size: 24px;font-weight: 600;color: #5b258f;"> Karina Bernabe</span></span>
+                                <br>
+                                <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;">  <?php echo $lang['home']['content_test_8']; ?></span>
                             </div>
-                            <div class="desc" style="font-size: 18px;line-height: 25px;">
-                            <?php echo $lang['home']['content_test_7']; ?>
-                            </div>
-                            <br/>
-                            <span class="date"><span style="font-size: 24px;font-weight: 600;color: #5b258f;"> Karina Bernabe</span></span>
-                            <br>
-                            <span style="font-size: 16px;font-weight: 400;line-height: 23px;line-height: 41px;font-style: italic;">  <?php echo $lang['home']['content_test_8']; ?></span>
-                            </p>
-                        </div>
+                        </p>
                     </li>
                 </ul>
                 </div>
+                <br>
+                <br>
+                <br>
+                <br>
             </div>
             <hr class="no_line" style="margin: 0 auto 80px;">
             <!-- One Full Row-->
