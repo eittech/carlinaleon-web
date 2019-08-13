70f2326d103e29fc4abb17cc31c603c88d312594
Jesus Laya <jesusgerard2008@gmail.com>
2019-08-13 12:08:09 -0400

    Arreglo del menu de idioma, se adapta a las pantallas pequeñas

--- menu.php
+++ menu.php
@@ -39,6 +39,46 @@
 
 
 ?>
+<style>
+    li {
+  float: left;
+}
+
+li a, .dropbtn {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1;}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
 <div class="menu_wrapper">
     <nav id="menu" class="menu-main-menu-container">
         <ul id="menu-main-menu" class="menu">
@@ -60,10 +100,25 @@
             <li class="<?php echo $current_page_item_6; ?>">
                 <a href="contact.php<?php echo $idiom; ?>"><span><?php echo $lang['menu']['url_6']; ?></span></a>
             </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn"><i class="icon-language"></i></a>
+                <div class="dropdown-content">
+                    <a class="nav-link smooth-scroll" href="index.php?lang=pt" style="color:#9a9aa3 !important;font-size: 11px !important;">
+                        <b>Portugues</b>
+                    </a>
+                    <a class="nav-link smooth-scroll" href="index.php?lang=en" style="color:#9a9aa3 !important;font-size: 11px !important;">
+                        <b>English</b>
+                    </a>
+                    <a class="nav-link smooth-scroll" href="index.php?lang=es" style="color:#9a9aa3 !important;font-size: 11px !important;">
+                        <b>Spanish</b>
+                    </a>
+                </div>
+            </li>
+            <li class="divider last-item"></li>
             <!-- Menu de idioma -->
-                <li style="display:block;" class="nav-item dropdown hidden-xs hidden-sm submenu last" id="li_language">
+                <!--<li style="display:block;" class="nav-item dropdown hidden-xs hidden-sm submenu last" id="li_language">
                     <a class="nav-link smooth-scroll dropdown-toggle count-info page-scroll" data-toggle="dropdown" href="#" title="Idioma">
-                        <!--<img src="images/language.png">-->
+                        
                         <span class="label label-warning" id="span_num_respuestas"><i class="icon-language"></i></span>
                     </a>
                     <ul class="dropdown-menu" style="background-color: rgb(93, 104, 174) !important; color: rgb(255, 255, 255) !important; display: none;">
@@ -92,8 +147,9 @@
                         <li class="divider last-item"></li>
                     </ul>
                     <span class="menu-toggle"></span>
-                </li>
+                </li>-->
             </ul>
+            <span class="menu-toggle"></span>
         
     </nav>
     <a class="responsive-menu-toggle" href="#"><i class="icon-menu"></i></a>
